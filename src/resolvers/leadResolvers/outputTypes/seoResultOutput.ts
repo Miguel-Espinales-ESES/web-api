e0fcac2ba77aae021824a4bfed682202b75d0e5d
Miguel-Espinales-ESES <mee@topfloormarketing.net>
2020-02-10 15:29:50 -0600

    updated seo cheker funtion

--- src/resolvers/leadResolvers/outputTypes/seoResultOutput.ts
+++ src/resolvers/leadResolvers/outputTypes/seoResultOutput.ts
@@ -1,62 +1,19 @@
 import { ObjectType, Field } from "type-graphql";
 
 @ObjectType()
-class headers {
-  @Field(() => [String])
-  h1: string[]
-  @Field(() => [String])
-  h2: string[]
-  @Field(() => [String])
-  h3: string[]
-  @Field(() => [String])
-  h4: string[]
-  @Field(() => [String])
-  h5: string[]
-  @Field(() => [String])
-  h6: string[]
-}
-
-@ObjectType()
-class performance {
-  @Field()
-  score: number
-}
-
-@ObjectType()
-class accessibility extends performance {
-}
-
-@ObjectType()
-class seo extends performance {
-}
-
-@ObjectType()
-class scores {
-  @Field()
-  performance: performance
-  @Field()
-  accessibility: accessibility
-  @Field()
-  seo: seo
-}
-
-@ObjectType()
 export default class SEOResult {
   @Field()
-  url: string
-
-  @Field()
   title: string
-
-  @Field(() => headers)
-  headers:headers
-  
   @Field()
   description: string
-  
+  @Field({ nullable: true })
+  author: string
+  @Field({ nullable: true })
+  keywords: string
   @Field()
-  isMobileFriendly: string
-
+  heading1: string
   @Field()
-  scores: scores
+  totalHeadings: number
+  @Field()
+  imgAccessibility: number
 }
