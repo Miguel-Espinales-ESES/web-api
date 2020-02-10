5eb3e0dc4f326b15b5755b9676a2ce5f33cbb904
Miguel-Espinales-ESES <mee@topfloormarketing.net>
2020-02-10 15:30:22 -0600

    uptada seo cheker ouputput

--- src/utils/reusableSnippets/seoChecker.ts
+++ src/utils/reusableSnippets/seoChecker.ts
@@ -1,30 +1,4 @@
-const SEOChecker = require('advanced-seo-checker');
-
-type result = {
-  url: string
-  title: string
-  headers: {
-    h1: string[]
-    h2: string[]
-    h3: string[]
-    h4: string[]
-    h5: string[]
-    h6: string[]
-  }
-  description: string
-  scores: {
-    performance: {
-      score: number
-    }
-    accessibility: {
-      score: number
-    }
-    seo: {
-      score: number
-    }
-    isMobileFriendly: string
-  }
-}
+const seochecker = require('seo-checker');
 
 export default class CheckSEO {
   url: string
@@ -36,12 +10,14 @@ export default class CheckSEO {
   }
 
   async checkSEO () {
-    const crawler = SEOChecker(this.url, {});
     return new Promise((resolve, reject) => {
-      crawler.analyze([this.url]).then((summary: any) => {
-        if (!summary.pages[0]) { reject('We were not able to collect data from your website') }
-        const response: result = summary.pages[0]
-        resolve(response)
+      seochecker.load(this.url, function (summary: any) {
+        if(!summary) { // response will be false on error
+          reject('We could not analyze your website. Contact us for a personal evalutaion')
+        } else {
+          const data = seochecker.meta(summary)
+          resolve(data)
+        }
       });
     })
   }
